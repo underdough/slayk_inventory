bc46585d81c86fcd779763ef0a5f5276180c8546
underdough <k3vinch3nl1@gmail.com>
2025-02-20 13:09:05 -0500

    Finalizando dependencias

--- views/dependencias.php
+++ views/dependencias.php
@@ -1,2 +1,9 @@
-<link rel="stylsheet" type="text/css" href="libs/alertify/format/alertify.css">
-<link rel="stylsheet" type="text/css" href="libs/alertify/format/alertify.css">
+<link rel="stylsheet" type="text/css" href="../libs/alertifyjs/css/alertify.css">
+<link rel="stylsheet" type="text/css" href="../libs/alertifyjs/css/themes/default.css">
+<link rel="stylsheet" type="text/css" href="../libs/bootstrap/css/bootstrap.css">
+<link rel="stylsheet" type="text/css" href="../libs/select2/css/select2.css">
+
+<script src = "libs\jquery-3.2.1.min.js"></script>
+<script src="libs/alertify/alertify.js"></script>
+<script src="libs/bootstrap/js/bootstrap.js"></script>
+<script src="libs/select2/js/select2.js"></script>
